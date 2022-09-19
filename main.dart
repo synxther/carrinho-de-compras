165e12ea034cc39b5641c3d9ac1f5db94096ac3a
synxther <synxther@protonmail.com>
2022-09-19 15:58:16 -0300

    Carro de compras em programação estrutural

---
+++ main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  List<String> produtos = [];
+  print("deseja comprar alguma coisa?");
+  String comprar = stdin.readLineSync()!.toUpperCase();
+  bool condicao = true;
+  if (comprar == "SIM") {
+    while (condicao) {
+      print("Qual produto?");
+      String produto = stdin.readLineSync()!;
+      produtos.add(produto);
+      print("produtos adicionados:${produtos.length}");
+
+      print("[1]- continuar [2]- imprimir [3]-sair");
+      String text = stdin.readLineSync()!.toUpperCase();
+      if (text == "3") {
+        condicao = false;
+      } else if (text == "2") {
+        print("esse(s) são os os produtos: ${produtos}");
+        print("deseja remover algum item?");
+        String remover = stdin.readLineSync()!.toUpperCase();
+        if (remover == "SIM" || remover == "S") {
+          print("qual produto?");
+
+          produto = stdin.readLineSync()!;
+          produtos.remove(produto);
+          print("produto removido com sucesso, deseja continuar comprando? Y/N");
+        }
+        text = stdin.readLineSync()!;
+        if (text == "N") {
+          condicao = false;
+          print("compra finalizada");
+        } else {
+          print("adicione novos produtos \n");
+        }
+      }
+    }
+  } else {
+    print("programa finalizado");
+    bool condicao = false;
+  }
+}
